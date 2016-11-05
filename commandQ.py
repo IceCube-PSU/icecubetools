b3a342ba45e1ed4148cfa5f550b332a873e36731
J.L. Lanfranchi <jll1062@phys.psu.edu>
2016-11-05 01:24:47 -0400

    basic commandQ version untested but should be close to working

--- commandQ.py
+++ commandQ.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
+
+from argparse import ArgumentParser
 import fcntl
 import os
-import popen
 import random
+import subprocess
 
 
-QUEUE_FPATH = os.path.expandvars(os.path.expanduser('~/commandQ.queue'))
-LOCK_FPATH = os.path.expandvars(os.path.expanduser('~/commandQ.lock'))
+QUEUE_FPATH = os.path.expandvars(os.path.expanduser('~/command.queue'))
+LOCK_FPATH = os.path.expandvars(os.path.expanduser('~/command.lock'))
 
 
-def get_command(self, timeout=10):
+def get_command(timeout):
     t0 = time.time()
     timeout_time = t0 + timeout
     while time.time() < timeout_time:
@@ -37,11 +39,67 @@ def get_command(self, timeout=10):
         finally:
             lockf.close()
 
+    # If you get here, then failed to get command
     raise TimeoutError('Failed to get a command in %s sec.' %timeout)
 
 
-#class Worker(object):
-#    def __init__(self):
-#        pass
+def run_command(command, cpu_threads=1, cpu_cores=None, gpus=None):
+    env = os.environ.copy()
 
-        
+    if cpu_cores is not None:
+        raise NotImplementedError('`cpu_cores`')
+
+    if gpus is not None:
+        if isinstance(gpus, int):
+            gpus = str(gpus)
+        elif isinstance(gpus, Sequence):
+            gpus = ','.join([str(n) for n in gpus])
+        if not isinstance(gpus, basestring):
+            raise ValueError('Unhandled `gpus`: "%s" of type %s'
+                             %(gpus, type(gpus)))
+        # export CUDA_VISIBLE_DEVICES=gpus
+        env['CUDA_VISIBLE_DEVICES'] = gpus
+
+    env['MKL_NUM_THREADS'] = format(cpu_threads, 'd')
+    env['OMP_NUM_THREADS'] = format(cpu_threads, 'd')
+
+    ret = subprocess.call(command, env=env, shell=True,
+                          stderr=subprocess.STDOUT)
+
+    return ret
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--gpu', type=str, required=True,
+        help='''Which gpu(s) to employ for all commands launched from this
+        process'''
+    )
+    parser.add_argument(
+        '--timeout', type=int, default=10,
+        help='''Default timeout (seconds)'''
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    while True:
+        print '='*80
+        print 'Getting a new command...'
+        command = get_command(args.timeout)
+        print 'Command to be run:\n    ' + command
+        print '='*80
+        t0 = time.time()
+        ret = run_command(command=command, gpus=args.gpu)
+        t1 = time.time()
+        print '='*80
+        print 'Command run time = %s sec' %(t1-t0)
+        print '\n\n'
+
+
+if __name__ == '__main__':
+    main()
